01b925814a946618a1767171c025b59337e61f5e
Aryaman Dubey <work.aryaman@gmail.com>
2022-07-05 00:28:36 +0530

    Created ros-to-esp led_toggle

---
+++ ros_esp_mr/esp_src/LED_toggle/LED_toggle.ino
@@ -0,0 +1,37 @@
+/* 
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
+ */
+
+#include <ros.h>
+#include <std_msgs/Empty.h>
+#define LED_BUILTIN 2
+
+ros::NodeHandle  nh;
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  if(digitalRead(LED_BUILTIN) == HIGH){
+    delay(2000);
+    digitalWrite(LED_BUILTIN, 0);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, 1);
+  }
+
+  // digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));   // blink the led
+}
+
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+
+void setup()
+{ 
+  pinMode(LED_BUILTIN, OUTPUT);
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{  
+  nh.spinOnce();
+  delay(1);
+}
